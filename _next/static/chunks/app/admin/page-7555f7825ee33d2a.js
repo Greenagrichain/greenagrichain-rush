5692e9585c51d420a056470e082504e60722c5b7
jstng <jjustcodeinn@gmail.com>
2024-09-02 17:40:56 +0100

    Redesign KYC UX

--- _next/static/chunks/app/admin/page-7555f7825ee33d2a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3,966,931],{18528:function(){}},function(n){n.O(0,[677,14,744],function(){return n(n.s=18528)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/admin/page-e13c3dc031df4be6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3,931,966],{18528:function(){}},function(n){n.O(0,[677,14,744],function(){return n(n.s=18528)}),_N_E=n.O()}]);
