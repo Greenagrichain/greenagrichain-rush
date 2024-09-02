85363a918ddafc5a45fe1ab006b5d2a9d37917c6
jstng <jjustcodeinn@gmail.com>
2024-09-02 14:31:08 +0100

    Redesign dashboard; use deposit and account-balance api

---
+++ _next/static/chunks/app/dashboard/plans/page-199bbba5850cd983.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[726],{44994:function(n,e,u){Promise.resolve().then(u.t.bind(u,12031,23))}},function(n){n.O(0,[31,677,14,744],function(){return n(n.s=44994)}),_N_E=n.O()}]);
--- _next/static/chunks/app/page-6ae1f39447ac6358.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,726],{44994:function(n,e,u){Promise.resolve().then(u.t.bind(u,12031,23))}},function(n){n.O(0,[31,677,14,744],function(){return n(n.s=44994)}),_N_E=n.O()}]);
