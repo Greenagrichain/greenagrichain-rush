af7acfa9d1ee4dffcbe057b564e5f8dc208539d8
jstng <jjustcodeinn@gmail.com>
2024-09-15 14:44:13 +0100

    Convert static about.html to react, refactor store.jsx into (static) route

---
+++ _next/static/chunks/app/admin/loading-6fa736e7aff65575.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4828,1931,4966,3,7009,7534],{18528:function(){}},function(n){n.O(0,[3677,3014,1744],function(){return n(n.s=18528)}),_N_E=n.O()}]);
